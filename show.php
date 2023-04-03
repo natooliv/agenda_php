9ae004c6c148f0ca56c0f899614b4b17be480064
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-04-03 20:31:16 -0300

    Subindo Projeto

---
+++ show.php
@@ -0,0 +1,18 @@
+<?php 
+  include_once("templates/header.php");
+?>
+  <div class="container" id="view-contact-container"> 
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title"><?= $contact["name"] ?></h1>
+    <p class="bold">Telefone:</p>
+    <p><?= $contact["phone"] ?></p>
+    <p class="bold">Observações:</p>
+    <p><?= $contact["observations"] ?></p>
+    <p class="bold">Foto:</p>
+    <?php if (!empty($contact["photo"])): ?>
+      <img src="<?= $BASE_URL . $contact["photo"] ?>" alt="<?= $contact["name"] ?>" width="150" height="150">
+    <?php endif; ?>
+  </div>
+<?php
+  include_once("templates/footer.php");
+?>
